3831ef25dfca7f871cde3985194171eaf26fb446
iordanov.serhii <sergey.ior97@gmail.com>
2016-08-15 13:13:58 +0300

    implement Contacts page

--- CarRental.WEB/Controllers/HomeController.cs
+++ CarRental.WEB/Controllers/HomeController.cs
@@ -4,7 +4,14 @@ namespace CarRental.WEB.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contacts()
         {
             return View();
         }
