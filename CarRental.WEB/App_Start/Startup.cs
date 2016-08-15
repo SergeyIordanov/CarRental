617c9ed8757b3945b9349873d926c01e7cff7569
iordanov.serhii <sergey.ior97@gmail.com>
2016-08-15 11:31:12 +0300

    Authentification system done

---
+++ CarRental.WEB/App_Start/Startup.cs
@@ -0,0 +1,28 @@
+﻿using CarRental.Auth.BLL.Interfaces;
+using CarRental.Auth.BLL.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace CarRental.WEB
+{
+    public class Startup
+    {
+        readonly IServiceCreator _serviceCreator = new ServiceCreator();
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(CreateUserService);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        private IUserService CreateUserService()
+        {
+            return _serviceCreator.CreateUserService("RentContext");
+        }
+    }
+}
