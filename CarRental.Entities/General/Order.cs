40cde64fc326815e15523240d874ac0950ac8720
iordanov.serhii <sergey.ior97@gmail.com>
2016-08-16 09:23:11 +0300

    add order entity

---
+++ CarRental.Entities/General/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Entities.General
+{
+    public class Order
+    {
+        public enum Status
+        {
+            Unwatched,
+            Declined,
+            Accepted,
+            Unpaid,
+            Paid,
+            Returned
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        public string PickUpAddress { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FromDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ToDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal TotalPrice { get; set; }
+
+        public bool WithDriver { get; set; }
+
+        public Status OrderStatus { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string DeclineIssue { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual Car Car { get; set; }
+    }
+}
